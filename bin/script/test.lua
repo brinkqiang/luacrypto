ccff44aca591fa1f5dce16e47b70c444941b9806
brinkqiang <brink.qiang@gmail.com>
2019-07-26 14:42:08 +0800

    add

--- bin/script/test.lua
+++ bin/script/test.lua
@@ -5,19 +5,19 @@ local rc = rc4.new()
 rc:SetKey("hello world")
 
 local str = rc:Encode("hello world")
-print(rc:Decode(str))
+print("rc -> " .. rc:Decode(str))
 
 local md5 = md5.new()
 local str2 = md5:GetMD5("hello world")
-print(str2)
+print("md5 -> " .. str2)
 
 local crc = crc.new()
 local str3 = crc:GetCRC("hello world")
-print(str3)
+print("crc -> " .. str3)
 
 local base64 = base64.new()
 local str4 = base64:Encode("hello world")
-print(base64:Encode(str4))
+print("base64 -> " .. base64:Decode(str4))
 
 local des = des.new()
 local DES3Block = DES3Block.new()
@@ -30,8 +30,8 @@ des:DESGenDecodeKey(DES3ContextDecKey, DES3Block)
 
 local encode = des:Encode(DES3ContextEncKey, DES3Block, "hello world")
 
-print(encode)
+print("des -> " .. encode)
 
 local decode = des:Decode(DES3ContextDecKey, DES3Block, encode)
 
-print(decode)
+print("des -> " .. decode)
