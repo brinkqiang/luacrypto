24762850441d4251b0f54d2d53832bfd56fe269c
brinkqiang <brink.qiang@gmail.com>
2019-07-26 14:51:07 +0800

    add

--- bin/script/test.lua
+++ bin/script/test.lua
@@ -1,28 +1,28 @@
 
-require("luacrypto")
+local luacrypto = require("luacrypto")
 
-local rc = rc4.new()
+local rc = luacrypto.rc4.new()
 rc:SetKey("hello world")
 
 local str = rc:Encode("hello world")
 print("rc -> " .. rc:Decode(str))
 
-local md5 = md5.new()
+local md5 = luacrypto.md5.new()
 local str2 = md5:GetMD5("hello world")
 print("md5 -> " .. str2)
 
-local crc = crc.new()
+local crc = luacrypto.crc.new()
 local str3 = crc:GetCRC("hello world")
 print("crc -> " .. str3)
 
-local base64 = base64.new()
+local base64 = luacrypto.base64.new()
 local str4 = base64:Encode("hello world")
 print("base64 -> " .. base64:Decode(str4))
 
-local des = des.new()
-local DES3Block = DES3Block.new()
-local DES3ContextEncKey = DES3Context.new()
-local DES3ContextDecKey = DES3Context.new()
+local des = luacrypto.des.new()
+local DES3Block = luacrypto.DES3Block.new()
+local DES3ContextEncKey = luacrypto.DES3Context.new()
+local DES3ContextDecKey = luacrypto.DES3Context.new()
 
 des:DESGenKey(DES3Block)
 des:DESGenEncodeKey(DES3ContextEncKey, DES3Block)
--- src/luacrypto/luacrypto_module.cc
+++ src/luacrypto/luacrypto_module.cc
@@ -27,50 +27,53 @@
 #include "dmcrc.h"
 #include "dmbase64.h"
 
+namespace lua_module
+{
+    static sol::table require_api(sol::this_state L) {
+        sol::state_view lua(L);
+        sol::table module = lua.create_table();
+        module.new_usertype<CDMRC>("rc4",
+            sol::constructors<CDMRC()>(),
+            "SetKey", &CDMRC::SetKey,
+            "Encode", sol::overload(sol::resolve<std::string(std::string&)>(&CDMRC::Encrypt), sol::resolve< char* (char* pBuf, size_t len)>(&CDMRC::Encrypt)),
+            "Decode", sol::overload(sol::resolve<std::string(std::string&)>(&CDMRC::Decrypt), sol::resolve< char* (char* pBuf, size_t len)>(&CDMRC::Decrypt))
+            );
+
+        module.new_usertype<CDMMD5>("md5",
+            sol::constructors<CDMMD5()>(),
+            "GetMD5", sol::overload(sol::resolve<std::string(std::string&)>(&CDMMD5::GetMD5))
+            );
+
+        module.new_usertype<DMDES3Context>("DES3Context");
+        module.new_usertype<DMDES3Block>("DES3Block");
+
+        module.new_usertype<CDMDes>("des",
+            sol::constructors<CDMDes()>(),
+            "DESGenKey", sol::overload(sol::resolve<void(DMDES3Block *pIV)>(&CDMDes::DESGenKey)),
+            "DESGenEncodeKey", sol::overload(sol::resolve<void(DMDES3Context *pCtx, DMDES3Block &oKey)>(&CDMDes::DESGenEncKeySche)),
+            "DESGenDecodeKey", sol::overload(sol::resolve<void(DMDES3Context *pCtx, DMDES3Block &oKey)>(&CDMDes::DESGenDecKeySche)),
+            "Encode", sol::overload(sol::resolve<std::string(DMDES3Context *pCtx, DMDES3Block *pIV, std::string& strInput)>(&CDMDes::Encode)),
+            "Decode", sol::overload(sol::resolve<std::string(DMDES3Context *pCtx, DMDES3Block *pIV, std::string& strInput)>(&CDMDes::Decode))
+            );
+
+        module.new_usertype<CDMCRC>("crc",
+            sol::constructors<CDMCRC()>(),
+            "GetCRC", sol::overload(&CDMCRC::GetCRC));
+
+        module.new_usertype<CDMBase64>("base64",
+            sol::constructors<CDMBase64()>(),
+            "Encode", sol::overload(&CDMBase64::Base64Encode),
+            "Decode", sol::overload(&CDMBase64::Base64Decode),
+            "hex2bin", sol::overload(&CDMBase64::hex2bin),
+            "bin2hex", sol::overload(&CDMBase64::bin2hex)
+            );
+
+        return module;
+    }
+}
 LUAMOD_API int luaopen_luacrypto(lua_State* L)
 {
-    luaL_Reg l[] = { 0 };
-
-    sol::state_view lua(L);
-    lua.new_usertype<CDMRC>("rc4",
-        sol::constructors<CDMRC()>(),
-        "SetKey", &CDMRC::SetKey,
-        "Encode", sol::overload(sol::resolve<std::string(std::string&)>(&CDMRC::Encrypt), sol::resolve< char* (char* pBuf, size_t len)>(&CDMRC::Encrypt)),
-        "Decode", sol::overload(sol::resolve<std::string(std::string&)>(&CDMRC::Decrypt), sol::resolve< char* (char* pBuf, size_t len)>(&CDMRC::Decrypt))
-        );
-
-    lua.new_usertype<CDMMD5>("md5",
-        sol::constructors<CDMMD5()>(),
-        "GetMD5", sol::overload(sol::resolve<std::string(std::string&)>(&CDMMD5::GetMD5))
-        );
-
-    lua.new_usertype<DMDES3Context>("DES3Context");
-    lua.new_usertype<DMDES3Block>("DES3Block");
-
-    lua.new_usertype<CDMDes>("des",
-        sol::constructors<CDMDes()>(),
-        "DESGenKey", sol::overload(sol::resolve<void(DMDES3Block *pIV)>(&CDMDes::DESGenKey)),
-        "DESGenEncodeKey", sol::overload(sol::resolve<void(DMDES3Context *pCtx, DMDES3Block &oKey)>(&CDMDes::DESGenEncKeySche)),
-        "DESGenDecodeKey", sol::overload(sol::resolve<void(DMDES3Context *pCtx, DMDES3Block &oKey)>(&CDMDes::DESGenDecKeySche)),
-        "Encode", sol::overload(sol::resolve<std::string(DMDES3Context *pCtx, DMDES3Block *pIV, std::string& strInput)>(&CDMDes::Encode)),
-        "Decode", sol::overload(sol::resolve<std::string(DMDES3Context *pCtx, DMDES3Block *pIV, std::string& strInput)>(&CDMDes::Decode))
-        );
-
-    lua.new_usertype<CDMCRC>("crc",
-        sol::constructors<CDMCRC()>(),
-        "GetCRC", sol::overload(&CDMCRC::GetCRC));
-
-    lua.new_usertype<CDMBase64>("base64",
-        sol::constructors<CDMBase64()>(),
-        "Encode", sol::overload(&CDMBase64::Base64Encode),
-        "Decode", sol::overload(&CDMBase64::Base64Decode),
-        "hex2bin", sol::overload(&CDMBase64::hex2bin),
-        "bin2hex", sol::overload(&CDMBase64::bin2hex)
-        );
-
-    luaL_newlib(L, l);
-
-    return 1;
+    return sol::stack::call_lua(L, 1, lua_module::require_api);
 }
 
 LUAMOD_API int require_luacrypto(lua_State* L)
